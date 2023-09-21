ad1870a2a6cd59f0a22a9896ff62e12b8052c2ea
serge loie <serge.loie@yandex.ru>
2023-09-21 20:47:06 +0500

    start project

---
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class SearchEngine {
+    public static List<String> search(List<Map<String, String>> docs, String str) {
+        return new ArrayList<>();
+    }
+
+}
---
+++ app/src/test/java/hexlet/code/SearchEngineTest.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchEngineTest {
+
+    @Test
+    void search() {
+    }
+}
