81e16db0d9ebc651a01439e02d11fe1540e6eb72
serge loie <serge.loie@yandex.ru>
2023-09-23 11:51:03 +0500

    предварительное завершение шага 1

--- app/src/main/java/hexlet/code/SearchEngine.java
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -3,10 +3,16 @@ package hexlet.code;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SearchEngine {
     public static List<String> search(List<Map<String, String>> docs, String str) {
-        return new ArrayList<>();
-    }
+        List<String> result = new ArrayList<>();
+        Pattern pattern = Pattern.compile("\\b" + Pattern.quote(str) + "\\b", Pattern.CASE_INSENSITIVE);
 
+        docs.stream()
+                .filter(x -> pattern.matcher(x.get("text")).find())
+                .forEach(x -> result.add(x.get("id")));
+        return result;
+    }
 }
--- app/src/test/java/hexlet/code/SearchEngineTest.java
+++ app/src/test/java/hexlet/code/SearchEngineTest.java
@@ -2,11 +2,30 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SearchEngineTest {
 
     @Test
     void search() {
+        var doc1 = "I can't shoot straight unless I've had a pint!";
+        var doc2 = "Don't shoot shoot shoot that thing at me.";
+        var doc3 = "I'm your shooter.";
+
+        List<Map<String, String>> docs = List.of(
+                Map.of("id", "doc1", "text", doc1),
+                Map.of("id", "doc2", "text", doc2),
+                Map.of("id", "doc3", "text", doc3)
+        );
+        List<String> result = SearchEngine.search(docs, "shoot");
+        List<String> expected = new ArrayList<>();
+        expected.add("doc1");
+        expected.add("doc2");
+        assertEquals(expected, result);
+        //System.out.println(result);
     }
 }
